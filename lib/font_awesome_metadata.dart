ce5668daf6fcdc9d216f9554edd28f97bac231fa
Dorin Balan <dorin.balan@medel.com>
2023-03-01 13:30:41 +0100

    initial

---
+++ lib/font_awesome_metadata.dart
@@ -0,0 +1,6 @@
+/// Search and filter data structure for FontAwesome Icons
+library font_awesome_metadata;
+
+export 'src/font_awesome_search.dart' show searchTermMappings;
+export 'src/font_awesome_named.dart' show faNamedMappings;
+export 'src/font_awesome_categories.dart' show FaIconCategory;
